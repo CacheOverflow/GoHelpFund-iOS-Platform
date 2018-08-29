bedc6f158592ed94603df6d46364548d8f1c2f10
Vlad Batrinu <batrinuvlad@gmail.com>
2018-08-29 19:39:05 +0300

    step3 fix;

--- GoHelpFund-Platform/Screens/CreateCampaign/Views/CreateCampaignStep3.swift
+++ GoHelpFund-Platform/Screens/CreateCampaign/Views/CreateCampaignStep3.swift
@@ -21,12 +21,17 @@ protocol CreateCampaignStep2Delegate {
     func didTapLocation()
 }
 
-class CreateCampaignStep3: NibView, DateTimePickerDelegate {
+class CreateCampaignStep3: NibView {
     @IBOutlet var startDateTextField: SkyFloatingLabelTextField!
-    
     @IBOutlet var endDateTextField: SkyFloatingLabelTextField!
-    
     @IBOutlet var locationTextField: SkyFloatingLabelTextField!
+    
+    var startDate: Date? = nil
+    var endDate: Date? = nil
+    
+    var location: String? {
+        return locationTextField.text
+    }
     
     let delegate: CreateCampaignStep2Delegate?
     
@@ -39,57 +44,46 @@ class CreateCampaignStep3: NibView, DateTimePickerDelegate {
     required init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
     }
-    
-    var startDate: Date {
-        return Date()
-    }
-    
-    var endDate: Date {
-        return Date()
-    }
-    
-    @IBAction func didBeginEditing(_ sender: Any) {
-        
-    }
-    
+
     func presentCalendar(with tag: Int) {
-        let min = Date().addingTimeInterval(-600 * 60 * 24 * 4)
-        let max = Date().addingTimeInterval(600 * 60 * 24 * 4)
+        let min = Date().addingTimeInterval(-6000 * 60 * 24 * 4)
+        let max = Date().addingTimeInterval(6000 * 60 * 24 * 4)
         let picker = DateTimePicker.create(minimumDate: min, maximumDate: max)
         picker.isDatePickerOnly = true
         
-        picker.includeMonth = true // if true the month shows at bottom of date cell
+        picker.includeMonth = true
         picker.highlightColor = UIColor(red: 255.0/255.0, green: 138.0/255.0, blue: 138.0/255.0, alpha: 1)
-        picker.darkColor = UIColor.darkGray
+        picker.darkColor = .darkGray
         picker.doneButtonTitle = "DONE"
-        picker.doneBackgroundColor = UIColor(red: 255.0/255.0, green: 138.0/255.0, blue: 138.0/255.0, alpha: 1)
+        picker.doneBackgroundColor = .green
         picker.completionHandler = { date in
             let formatter = DateFormatter()
-            formatter.dateFormat = "hh:mm aa dd/MM/YYYY"
+            formatter.dateFormat = "dd/MM/YYYY"
             
             if let fieldsEnum = TextFieldType(rawValue: tag) {
                 switch fieldsEnum {
                 case .startDate:
                     self.startDateTextField.text = formatter.string(from: date)
+                    self.startDate = date
                 case .endDate:
                     self.endDateTextField.text = formatter.string(from: date)
+                    self.endDate = date
                 case .location:
                     break
                 }
             }
         }
-        
-        picker.delegate = self
-        
-        picker.show()
-    }
     
-    func dateTimePicker(_ picker: DateTimePicker, didSelectDate: Date) {
-        
+        picker.show()
     }
     
     func update(with location: String) {
         locationTextField.text = location
+    }
+    
+    override var isValidStep: Bool {
+        guard let _ = startDate, let _ = endDate, let _ = location else { return false }
+        return true
     }
 }
 
@@ -105,11 +99,5 @@ extension CreateCampaignStep3: UITextFieldDelegate {
         }
         
         return false
-    }
-}
-    
-extension CreateCampaignStep3 {
-    var isValidStep: Bool {
-        return true
     }
 }
