8d21c68f20dd4b7fd9c36c7480e176a4212c101a
Vlad Batrinu <batrinuvlad@gmail.com>
2018-09-14 12:44:42 +0300

    adjust variable name;

--- GoHelpFund-Platform/AppDelegate.swift
+++ GoHelpFund-Platform/AppDelegate.swift
@@ -24,8 +24,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         
-        GMSPlacesClient.provideAPIKey(GOOGLE_CONSTANTS.GOOGLE_KEY)
-        GMSServices.provideAPIKey(GOOGLE_CONSTANTS.GOOGLE_KEY)
+        GMSPlacesClient.provideAPIKey(GOOGLE_CONSTANTS.KEY)
+        GMSServices.provideAPIKey(GOOGLE_CONSTANTS.KEY)
         IQKeyboardManager.shared.enable = true
         Fabric.with([Crashlytics.self])
         
